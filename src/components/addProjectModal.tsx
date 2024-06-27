751fecc473da135549cd8e1cd53093480aafc422
a.golovin <a.golovin@iq-adv.ru>
2024-06-27 14:17:38 +0500

    jira | fix: biome formatter

--- src/components/addProjectModal.tsx
+++ src/components/addProjectModal.tsx
@@ -1,14 +1,19 @@
 import { useAddJiraProject } from "@/api/jira/hooks/use-projects";
 import { CustomModal } from "@/shared/ui/custom-modal";
-import { Input } from "@/shared/ui/input"
-import { Label } from "@/shared/ui/label"
+import { Input } from "@/shared/ui/input";
+import { Label } from "@/shared/ui/label";
 import { useState, type FC } from "react";
 import { Interceptor } from "./interceptor";
 import { useJiraUsers } from "@/api/jira/hooks/use-users";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/shared/ui/select";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/shared/ui/select";
 import { useJiraStore } from "@/store/jira-store";
 import { toast } from "react-toastify";
-
 
 interface IModalProps {
 	title: string;
@@ -17,87 +22,87 @@ interface IModalProps {
 }
 
 export const AddProjectModal: FC<IModalProps> = () => {
-  const [formState, setFormState] = useState({name: "", key: ""});
+	const [formState, setFormState] = useState({ name: "", key: "" });
 
-  const {data, error, status} = useJiraUsers();
-  const { mutateAsync: addProject } = useAddJiraProject();
+	const { data, error, status } = useJiraUsers();
+	const { mutateAsync: addProject } = useAddJiraProject();
 
-  const selectedUserId = useJiraStore((store) => store.selectedJiraUser );
-	const setSelectedUser = useJiraStore(
-		(state) => state.setSelectedJiraUser,
+	const selectedUserId = useJiraStore((store) => store.selectedJiraUser);
+	const setSelectedUser = useJiraStore((state) => state.setSelectedJiraUser);
+
+	const userSelectOptions = data
+		?.filter(({ accountType }) => accountType !== "app")
+		.map((user) => (
+			<SelectItem value={String(user?.accountId)} key={user?.accountId}>
+				{user?.displayName}
+			</SelectItem>
+		));
+
+	return (
+		<CustomModal
+			confirmButtonText="Создать"
+			title="Создать проект"
+			onConfirm={async () => {
+				if (selectedUserId) {
+					return await addProject({
+						key: formState.key,
+						leadAccountId: selectedUserId,
+						name: formState.name,
+						projectTypeKey: "software",
+					});
+				}
+				toast("Не выбран руководитель проекта");
+			}}
+			toggleButtonText="Создайте проект"
+		>
+			<div className="grid gap-4 py-4">
+				<div className="grid grid-cols-4 items-center gap-4">
+					<Label htmlFor="name" className="text-right">
+						Название
+					</Label>
+					<Input
+						id="name"
+						className="col-span-3"
+						value={formState.name}
+						onChange={(e) =>
+							setFormState({ ...formState, name: e.target.value })
+						}
+					/>
+				</div>
+				<div className="grid grid-cols-4 items-center gap-4">
+					<Label htmlFor="key" className="text-right">
+						KEY
+					</Label>
+					<Input
+						id="key"
+						className="col-span-3"
+						value={formState.key}
+						onChange={(e) =>
+							setFormState({ ...formState, key: e.target.value })
+						}
+					/>
+				</div>
+				<Interceptor status={status} errorMessage={error?.message}>
+					<div className="grid grid-cols-4 items-center gap-4">
+						<Label htmlFor="username" className="text-right">
+							Руководитель
+						</Label>
+						<Select
+							onValueChange={setSelectedUser}
+							value={
+								data?.some((user) => String(user.accountId) === selectedUserId)
+									? selectedUserId
+									: undefined
+							}
+						>
+							<SelectTrigger className="w-[180px]">
+								<SelectValue placeholder="Выберите проект" />
+							</SelectTrigger>
+							<SelectContent>{userSelectOptions}</SelectContent>
+						</Select>
+					</div>
+				</Interceptor>
+			</div>
+		</CustomModal>
 	);
-
-  const userSelectOptions = data?.filter(({ accountType }) => accountType !== "app").map((user) => (
-		<SelectItem value={String(user?.accountId)} key={user?.accountId}>
-			{user?.displayName}
-		</SelectItem>
-	));
-
-  return (
-			<CustomModal 
-				confirmButtonText="Создать"
-				title="Создать проект"
-				onConfirm={async () => {
-          if (selectedUserId) {
-            return await addProject({
-              key: formState.key,
-              leadAccountId: selectedUserId,
-              name: formState.name,
-              projectTypeKey: "software"
-            })
-          }
-          toast("Не выбран руководитель проекта")
-        }}
-				toggleButtonText="Создайте проект"
-			>
-
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Название
-              </Label>
-              <Input
-                id="name"
-                className="col-span-3"
-                value={formState.name}
-                onChange={(e) => setFormState({...formState, name: e.target.value})}
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="key" className="text-right">
-                KEY
-              </Label>
-              <Input
-                id="key"
-                className="col-span-3"
-                value={formState.key}
-                onChange={(e) => setFormState({...formState, key: e.target.value})}
-              />
-            </div>
-            <Interceptor status={status} errorMessage={error?.message}>
-					    <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Руководитель
-                </Label>
-                <Select
-								onValueChange={setSelectedUser}
-								value={
-									data?.some(
-										(user) => String(user.accountId) === selectedUserId,
-									)
-										? selectedUserId
-										: undefined
-								}
-						    > 
-								  <SelectTrigger className="w-[180px]">
-									  <SelectValue placeholder="Выберите проект" />
-								  </SelectTrigger>
-								  <SelectContent>{userSelectOptions}</SelectContent>
-							  </Select>
-              </div>
-            </Interceptor>
-        </div>
-
-			</CustomModal>
-  )
-}
+};
--- src/shared/ui/custom-modal.tsx
+++ src/shared/ui/custom-modal.tsx
@@ -1,7 +1,14 @@
 import type { FC, ReactNode } from "react";
-import { Button } from "./button"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./dialog"
-
+import { Button } from "./button";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "./dialog";
 
 interface ICustomModal {
 	title: string;
@@ -13,25 +20,30 @@ interface ICustomModal {
 }
 
 export const CustomModal: FC<ICustomModal> = ({
-	title, onConfirm, description, toggleButtonText, confirmButtonText, children
+	title,
+	onConfirm,
+	description,
+	toggleButtonText,
+	confirmButtonText,
+	children,
 }) => {
-  return (
+	return (
 		<Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">{toggleButtonText}</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>
-            {description}
-          </DialogDescription>
-        </DialogHeader>
-					{children}
-        <DialogFooter>
-          <Button type="submit" onClick={onConfirm}>{confirmButtonText}</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-	)
-}
+			<DialogTrigger asChild>
+				<Button variant="outline">{toggleButtonText}</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>{title}</DialogTitle>
+					<DialogDescription>{description}</DialogDescription>
+				</DialogHeader>
+				{children}
+				<DialogFooter>
+					<Button type="submit" onClick={onConfirm}>
+						{confirmButtonText}
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+};
